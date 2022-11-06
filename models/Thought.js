0a5aba1dfe79c15a925821c03b0b67203d27a16b
Esra Al <esra.wameed@yahoo.com>
2022-11-06 16:57:27 -0500

    deleted Reaction model and put as subdocument of thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,21 @@
 const {Schema,model,Types} = require('mongoose');
+
+//Reaction schema only, mongoose subdocument
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+          },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+          },
+        username: { type: String, required: true },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+          },
+    }
+)
