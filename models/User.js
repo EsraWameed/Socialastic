1a18167b29e339584b9863720f3fa9ebda843bff
Esra Al <esra.wameed@yahoo.com>
2022-11-06 18:40:12 -0500

    added user model with thoughts and friends referencing thoughts and self reference user

--- models/User.js
+++ models/User.js
@@ -1,1 +1,39 @@
 const {Schema,model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        //string, unique, requried, trimmed
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+          },
+          // string, required, unique, must match valid email address
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            validate: {
+                validator: function(v) {
+                    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+                },
+                message: "Please enter a valid email"
+            },
+        },
+        thoughts: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'thought',
+            },
+          ],
+          friends: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'user',
+            },
+          ],
+    }
+)
+//a virtual called friendCount that retreives length of the user's friends array field on query
